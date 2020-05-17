3d9bb199cee3a8daca8d57c6d23083ecc68171ce
ZaqueuRodrigues <zakeurodrigues@outlook.com>
2020-05-17 17:54:34 -0300

    implementando classe Order

--- src/main/java/com/course/entities/User.java
+++ src/main/java/com/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
@@ -20,6 +25,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -73,9 +81,9 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
@@ -102,5 +110,7 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+
 
 }
