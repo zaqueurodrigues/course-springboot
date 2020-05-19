0d9a857d709b27187cbe5b42da7e825b45c71e21
ZaqueuRodrigues <zakeurodrigues@outlook.com>
2020-05-19 16:15:26 -0300

    classe Product

---
+++ src/main/java/com/course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.repositories.ProductRepository;
+import com.course.entities.Product;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
