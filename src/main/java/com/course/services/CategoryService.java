b97fcb92859a81c9d270f459326be485875ac36d
ZaqueuRodrigues <zakeurodrigues@outlook.com>
2020-05-19 14:43:47 -0300

    classe Category

---
+++ src/main/java/com/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.repositories.CategoryRepository;
+import com.course.entities.Category;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
