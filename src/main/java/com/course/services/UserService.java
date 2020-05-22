094b297502697425f66a923294c7276641bb5aef
ZaqueuRodrigues <zakeurodrigues@outlook.com>
2020-05-22 11:36:46 -0300

    Class FIndById Exception Handling with Carla helps

--- src/main/java/com/course/services/UserService.java
+++ src/main/java/com/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.course.repositories.UserRepository;
+import com.course.services.exceptions.ResourceNotFoundException;
 import com.course.entities.User;
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
